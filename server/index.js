0209169adb4bf1f6b4abd7250dae48529e85a565
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-07-28 02:10:26 +0300

    get started

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes/posts.js";
+import router from "../server/routes/posts";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
