8cac316e9716f6f03702425522d9014eb924d22a
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-07-28 03:04:58 +0300

    get started

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/posts";
+const url = "https://souvenir-01.herokuapp.com/posts";
 export const fetchPosts = () => axios.get(url);
 export const createPost = (post) => axios.post(url, post);
 export const updatePost = (id, updatedPost) =>
--- server/index.js
+++ server/index.js
@@ -11,11 +11,11 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// app.get("/", (req, res) => {
-//   res.send({
-//     message: "Welcome to memory app",
-//   });
-// });
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to memory app",
+  });
+});
 app.use("/posts", routes);
 
 const PORT = process.env.PORT || 5000;
