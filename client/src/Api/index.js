f2f6c014a31301a098cbd947d3d9f0e883c4ae88
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-08-04 12:32:24 +0300

    Auth

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -8,5 +8,5 @@ export const updatePost = (id, updatedPost) =>
 export const deletePost = (id) => API.delete(`posts/${id}`);
 export const likePost = (id) => API.patch(`posts/${id}/like`);
 
-export const signIn = (formData) => API.post("/users/signin", formData);
-export const signUp = (formData) => API.post("/users/signup", formData);
+export const signIn = (formData) => API.post("/user/signin", formData);
+export const signUp = (formData) => API.post("/user/signup", formData);
