4677d26168f45c3a2fbcb73b41d813033bcd7964
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-08-09 14:07:11 +0300

    fix pagination error

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // https://souvenir-01.herokuapp.com/
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://souvenir-01.herokuapp.com" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${
