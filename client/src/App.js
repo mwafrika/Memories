d6de33a885af10615cd7a9cff973f28d3093ff66
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-03-17 16:57:41 +0200

    Fix issues in css

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,31 @@
-import React from "react";
-import { Container } from "@material-ui/core";
-import NavBar from "./components/NavBar/Navbar";
-import Home from "./components/Home/Home";
-import Auth from "./components/Auth/Auth";
-import PostDetails from "./components/postDetails/PostDetails";
+import React from 'react';
+import './background.css';
+import { Container } from '@material-ui/core';
+import NavBar from './components/NavBar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import PostDetails from './components/postDetails/PostDetails';
 import {
   Route,
   BrowserRouter as Router,
   Switch,
   Redirect,
-} from "react-router-dom";
+} from 'react-router-dom';
 const App = () => {
-  const user = JSON.parse(localStorage.getItem("profile"));
+  const user = JSON.parse(localStorage.getItem('profile'));
   return (
     <Router>
-      <Container maxWidth="xl">
+      <Container maxWidth='xl'>
         <NavBar />
         <Switch>
-          <Route path="/" exact component={() => <Redirect to="/posts" />} />
-          <Route path="/posts" exact component={Home} />
-          <Route path="/posts/search" exact component={Home} />
-          <Route path="/posts/:id" component={PostDetails} />
+          <Route path='/' exact component={() => <Redirect to='/posts' />} />
+          <Route path='/posts' exact component={Home} />
+          <Route path='/posts/search' exact component={Home} />
+          <Route path='/posts/:id' component={PostDetails} />
           <Route
-            path="/Auth"
+            path='/Auth'
             exact
-            component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}
+            component={() => (!user ? <Auth /> : <Redirect to='/posts' />)}
           />
         </Switch>
       </Container>
